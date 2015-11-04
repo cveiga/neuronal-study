a37b2b25dc24e7b4a57aaa482946b797669609fa
Carlos Veiga Almagro <krlosva82@gmail.com>
2015-11-05 00:59:28 +0100

    new common vocabulary for all images

--- descomponerImagen.cpp
+++ descomponerImagen.cpp
@@ -9,7 +9,10 @@ cv::Mat _vCluster;
 cv::Mat _img;
 cv::Mat _centers;
 
+int _numeroImagenes;
 
+int cuentaImagenes(char* nomFich);
+std::string leerImagen(std::ifstream &fich, std::string &ruta, std::string &imagen);
 void creaVectorImagen(int fila, int columna);
 void creaVectorPuntos(int fila, int columna);
 void rellenaVectorImagen();
@@ -17,7 +20,6 @@ void rellenaVectorPuntos(int fila, int f, int c, cv::Mat);
 void rellenaMiHistograma(MiHistograma &mh);
 void imprimeVectorImagen();
 void imprimeVCluster();
-//void muestraHistograma();
 void eliminaVectorImagen();
 void eliminaVectorPuntos();
 
@@ -30,6 +32,9 @@ int main (int argc, char* argv[])
 		exit(1);
 	}
 
+	_numeroImagenes = cuentaImagenes(argv[argc-1]); 
+	std::cout << "TOTAL IMAGENES: " << _numeroImagenes << std::endl;
+
 	std::ifstream fich(argv[argc-1]);
 
 	if (!fich)
@@ -37,30 +42,25 @@ int main (int argc, char* argv[])
 		std::cout << "Error no se puede abrir el archivo: " << std::endl;
         exit(1);
 	}
+
 	std::string ruta;
 	std::string imagen;
+	std::string nameImage;
+
+	cv::Mat data;
 
 	getline(fich, ruta);
 
 	long posicion = fich.tellg();;
 	char basura;
+	int filaVectorPuntos = 0;
 
 	do{
 		fich.seekg(posicion, fich.beg);
-		getline(fich, imagen);
 
-		if (!imagen.find("/"))	//nueva ruta de acceso a las imágenes
-		{
-			ruta = imagen;
-			getline(fich, imagen);
-		}
+		nameImage = leerImagen(fich, ruta, imagen);
 
-		std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
-		std::cout << ruta << std::endl;
-
-		std::string nameImage = imagen.substr(0, imagen.size()-4);	//imagen.find(".png"));
-		cv::namedWindow(imagen.substr(0, imagen.size()-4), cv::WINDOW_AUTOSIZE);
-		//cv::namedWindow(imagen.substr(0, imagen.find(".png")), cv::WINDOW_AUTOSIZE);
+		//cv::namedWindow(imagen.substr(0, imagen.size()-4), cv::WINDOW_AUTOSIZE);
 		
 		const std::string & path = "../" + ruta.substr(ruta.find("IMG"), ruta.length()) + "/" + imagen;
 		std::cout << "PATH: " << path << std::endl;
@@ -68,59 +68,32 @@ int main (int argc, char* argv[])
 		_img = cv::imread(path, cv::IMREAD_ANYCOLOR | cv::IMREAD_ANYDEPTH);
 		if (_vImage == NULL) creaVectorImagen(_img.rows, _img.cols);
 
-		//cv::imshow(imagen.substr(0, imagen.find(".png")), _img);
-		cv::imshow(imagen.substr(0, imagen.size()-4), _img);
-		std::cout << "NO LLEGA" << std::endl;
+		/** 9 columnas, por que utilizamos una mascara de 3x3, que genera 9 coordenadas por cada pixel
+		**  los pixeles de los extremos se descartan */
+		if(data.empty()) data = cv::Mat::zeros(((_img.rows - 1) * (_img.cols - 1) * _numeroImagenes), 9, CV_32FC1);
+		if (_vPoints == NULL) creaVectorPuntos((((_img.rows - 1) * (_img.cols - 1)) * _numeroImagenes), 9);
 
-		cv::waitKey(1);
+		//cv::imshow(imagen.substr(0, imagen.size()-4), _img);
+		//std::cout << "NO LLEGA" << std::endl;
+
+		//cv::waitKey(1);
 
 		rellenaVectorImagen();
 
 		std::cout << _img.dims << std::endl;
 
-		/** 9 columnas, por que utilizamos una mascara de 3x3, que genera 9 coordenadas por cada pixel
-		**  los pixeles de los extremos se descartan */
-		cv::Mat data = cv::Mat::zeros((_img.rows - 1) * (_img.cols - 1), 9, CV_32FC1);
-		if (_vPoints == NULL) creaVectorPuntos((_img.rows - 1) * (_img.cols - 1), 9);
-
-		int cont = 0;
-		for (int f = 1; f < 399; f++){
+		for (int f = 1; f < 399; f++)
+		{ 
 			for (int c = 1; c < 399; c++)
 			{
-				rellenaVectorPuntos(cont++, f, c, data);
+				rellenaVectorPuntos(filaVectorPuntos++, f, c, data);
 			}
 		}
 
-		std::cout << "TOTAL: " << cont << std::endl;
-		//std::cout << "TIPO: -> " << imagen.substr(imagen.find_last_of(" "), imagen.find(".png")) << std::endl;
+		std::cout << "TOTAL: " << filaVectorPuntos << std::endl;
 		std::cout << "TIPO: -> " << nameImage.substr(nameImage.find_last_of(" "), nameImage.size()) << std::endl;
-		//imprimeVectorImagen();
-		//cv::Mat indices, dists;
-		double * dists;
-		double compactness = cv::kmeans(data, NCLUSTER, _vCluster, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 10, 0.01), 3, cv::KMEANS_PP_CENTERS, _centers);
-		//cv::parallel_for_(cv::Range(0, NCLUSTER), KMeansDistanceComputer(dists, _vCluster, data, _centers));
-		//cv::ParallelLoopBody::kmeansPPDistanceComputer();
-		//int::knnSearch(data, indices, dists, NCLUSTER, 32);
 
-		std::cout << "Filas de _centers: " << _centers.rows << ", Columnas de _centers: " << _centers.cols << std::endl;
-		std::cout << "Filas de _vCluster: " << _vCluster.rows << ", Columnas de _vCluster: " << _vCluster.cols << std::endl;
-
-		MiHistograma mHisto(NCLUSTER, _centers.cols, nameImage.substr(nameImage.find_last_of(" "), nameImage.size()));
-		rellenaMiHistograma(mHisto);
-		mHisto.imprimeHistograma();
-		mHisto.saveHistograma();
-		/*cv::waitKey(0);
-		for(int i = 0; i < _centers.rows; i++)
-		{
-			std::cout << "   " << i+1 << "\t  ===>  ";
-			for (int j = 0; j < _centers.cols; j++)
-			{
-				std::cout << " [" << _centers.at<float>(i,j) << "] -";
-			}
-			std::cout << std::endl;
-		}*/
-		//cv::destroyWindow(imagen.substr(0, imagen.find(".png")));
-		cv::destroyWindow(imagen.substr(0, imagen.size()-4));
+		//cv::destroyWindow(imagen.substr(0, imagen.size()-4));
 
 		posicion = fich.tellg();
 		fich >> basura;
@@ -128,6 +101,13 @@ int main (int argc, char* argv[])
 	//std::cout << "Total De Totales: " << mHisto.getTotaltes() << std::endl;
 
 	fich.close();
+
+	double compactness = cv::kmeans(data, NCLUSTER, _vCluster, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 10, 0.01), 3, cv::KMEANS_PP_CENTERS, _centers);
+
+	MiHistograma mHisto(NCLUSTER, _centers.cols, nameImage.substr(nameImage.find_last_of(" "), nameImage.size()));
+	rellenaMiHistograma(mHisto);
+	mHisto.imprimeHistograma();
+	mHisto.saveHistograma();
 
 	eliminaVectorImagen();
 	eliminaVectorPuntos();
@@ -137,8 +117,47 @@ int main (int argc, char* argv[])
 
 
 
+int cuentaImagenes(char* nomFich)
+{
+	std::ifstream fich(nomFich);
+
+	if (!fich)
+	{
+		std::cout << "Error no se puede abrir el archivo: " << std::endl;
+        exit(1);
+	}
+	std::string linea;
+	int totalImagenes = 0, totalRutas = 0;
+	
+	while(!fich.eof())
+	{
+		getline(fich, linea);
+		if (!linea.find("/")) totalRutas++;
+		else totalImagenes++;
+	}
+
+	fich.close();
+
+	return totalImagenes -  totalRutas;
+}
 
 
+
+std::string leerImagen(std::ifstream &fich, std::string &ruta, std::string &imagen)
+{
+	getline(fich, imagen);
+
+	if (!imagen.find("/"))	//nueva ruta de acceso a las imágenes
+	{
+		ruta = imagen;
+		getline(fich, imagen);
+	}
+
+	std::cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+	std::cout << ruta << std::endl;
+
+	return imagen.substr(0, imagen.size()-4);
+}
 
 
 void creaVectorImagen(int fila, int columna)
@@ -149,7 +168,7 @@ void creaVectorImagen(int fila, int columna)
 }
 
 void creaVectorPuntos(int fila, int columna)
-{
+{  std::cout << "FILA: " << fila << ", COLUMNA: " << columna << std::endl;
 	_vPoints = new ushort*[fila];
 	for (int i = 0; i < fila; i++)
 		_vPoints[i] = new ushort[columna];
@@ -186,12 +205,12 @@ void rellenaVectorImagen()
 		if (mostrado >= 400) break;		
 		if (cont < 1) mostrar = true;
 		cont++;
-	}
+	}std::cout << "SALIMOS" << std::endl;
 }
 
 /** En esta función se almacenan el valor de cada uno de los puntos de 9 pixeles resultantes de aplicar una mascara de 3x3 */
 void rellenaVectorPuntos(int fila, int f, int c, cv::Mat data)
-{
+{	
 	data.at<float>(fila, 0) = _vPoints[fila][0] = _vImage[f - 1][c - 1];
 	data.at<float>(fila, 1) = _vPoints[fila][1] = _vImage[f - 1][c];
 	data.at<float>(fila, 2) = _vPoints[fila][2] = _vImage[f - 1][c + 1];
@@ -246,49 +265,6 @@ void imprimeVCluster()
 	}
 }
 
-/*void muestraHistograma()
-{
- 	/// Establish the number of bins
-  	int histSize = 100;
-
-  	/// Set the ranges ( for B,G,R) )
-  	float range[] = { 1, 100 } ;
-  	const float* histRange = { range };
-
-	cv::Mat miHistograma;
-	cv::calcHist(&_centers, 1, 0, cv::Mat(), miHistograma, 1, &histSize, &histRange, true, false);
-	//cv::calcHist(_centers, 1, 0, cv::Mat(), miHistograma, 1, &histSize, &histRange, uniform, accumulate );
-
-	for( int h = 0; h < histSize; h++ )
-    {
-            float binVal = miHistograma.at<float>(h);
-            std::cout<<" "<<binVal << ",";
-    }
-    std::cout << std::endl;
-
-	// Draw the histograms for R, G and B
-	int hist_w = 512; int hist_h = 400;
-	int bin_w = cvRound( (double) hist_w/histSize );
-
-	cv::Mat histImage( hist_h, hist_w, CV_8UC1);
-
-	/// Normalize the result to [ 0, histImage.rows ]
-	cv::normalize(miHistograma, miHistograma, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
-
-	/// Draw for each channel
-	for( int i = 1; i < histSize; i++ )
-	{
-    	cv::line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(miHistograma.at<float>(i-1)) ) ,
-                     cv::Point( bin_w*(i), hist_h - cvRound(miHistograma.at<float>(i)) ),
-                     cv::Scalar( 255, 0, 0), 2, 8, 0  );
-    }
-
-	cv::namedWindow("calcHist Demo", cv::WINDOW_AUTOSIZE );
-	cv::imshow("calcHist Demo", histImage );
-
-	cv::waitKey(0);
-}*/
-
 void eliminaVectorImagen()
 {
 	for (int i = 0; i < _img.rows; i++)
@@ -298,7 +274,7 @@ void eliminaVectorImagen()
 
 void eliminaVectorPuntos()
 {
-	for (int i = 0; i < (_img.rows - 1) * (_img.cols - 1); i++)
+	for (int i = 0; i < (_img.rows - 1) * (_img.cols - 1) * _numeroImagenes; i++)
 		delete[] _vPoints[i];
 	delete[] _vPoints;
 }
