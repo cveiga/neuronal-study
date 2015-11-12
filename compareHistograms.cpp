8df0a480d56e0b6256b619711118da379a516196
CVeiga <krlosva82@gmail.com>
2015-11-12 21:35:41 +0100

    generator of confusion matrix and compare histograms from k-neighbors

--- compareHistograms.cpp
+++ compareHistograms.cpp
@@ -1,12 +1,23 @@
 #include "head.h"
 
-#define NCLUSTERS 1000
+#define NCLUSTERS 500	//1000
+#define KVECINOS 5		//5 mínimo para propiciar la repetición
+
+/** Estructura para los k-vecinos */
+struct Vecinos
+{
+	double distancia;
+	std::string tipoVecino;
+};
 
 
 std::string leerHistograma(int*, std::string &, std::ifstream &);
-void guardaResultado(std::string, std::string, std::string);
+void guardaResultado(std::string, std::string, std::string, std::string);
 void compararHistogramas(int *, int *, long &, std::string &, std::string);
-void distanciaEuclidea(int *, int *, double &, std::string &, std::string);
+double distanciaEuclidea(int *, int *, double &, std::string &, std::string);
+void encuentraKVecinos(Vecinos *, int &, const std::string, const double);
+std::string votaVecino(Vecinos *);
+void imprimeKVecinos(Vecinos *);
 bool seguridad(int argc, char* argv[], std::ifstream &fichTest)
 {
 	if (argc < 3) 
@@ -31,6 +42,7 @@ int main(int argc, char *argv[])
 {
 	std::ifstream fichTest, fichTraining;
 	long aciertos = 0;
+	Vecinos vecinosMasProximos[KVECINOS];
 
 	if (!seguridad(argc, argv, fichTest)) return 0;
 
@@ -43,7 +55,9 @@ int main(int argc, char *argv[])
 	{
 		long totalCoincidencias = 0;
 		double minimo = -1;
-		std::string pertenece, perteneceEuclidea;
+		int posicionVecino = 0;
+		std::string pertenece, perteneceEuclidea, perteneceKVecinos;
+		for (int i = 0; i < KVECINOS; i++) vecinosMasProximos[i].distancia = 0;
 
 		if (posicionTest != 0) fichTest.seekg(posicionTest, fichTest.beg);
 		std::string tipoTest = leerHistograma(hisTest, imageTest, fichTest);
@@ -63,22 +77,25 @@ int main(int argc, char *argv[])
 			std::string tipoTraining = leerHistograma(hisTraining, imageTraining, fichTraining);
 			//std::cout << "\tTRAINING: " <<  tipoTraining << std::endl;
 		
-			compararHistogramas(hisTest, hisTraining, totalCoincidencias, pertenece, tipoTraining);
-			distanciaEuclidea(hisTest, hisTraining, minimo, perteneceEuclidea, tipoTraining);
-
+			//compararHistogramas(hisTest, hisTraining, totalCoincidencias, pertenece, tipoTraining);
+			encuentraKVecinos(vecinosMasProximos, posicionVecino, tipoTraining, distanciaEuclidea(hisTest, hisTraining, minimo, perteneceEuclidea, tipoTraining));
+			//imprimeKVecinos(vecinosMasProximos);
 			posicionTraining = fichTraining.tellg();
 			fichTraining >> basura;
 		}
 		fichTraining.close();
-		guardaResultado(imageTest.substr(imageTest.find_last_of("/"), imageTest.size()), pertenece, perteneceEuclidea);
+		perteneceKVecinos = votaVecino(vecinosMasProximos);
+		guardaResultado(imageTest.substr(imageTest.find_last_of("/"), imageTest.size()), pertenece, perteneceEuclidea, perteneceKVecinos);
 
-		if(tipoTest == pertenece) aciertos++;
-		if(tipoTest == perteneceEuclidea) aciertos++;
+		//if(tipoTest == pertenece) aciertos++;
+		//if(tipoTest == perteneceEuclidea) aciertos++;
+		if(tipoTest == perteneceKVecinos) aciertos++;
 
 		posicionTest = fichTest.tellg();
 		fichTest >> basura;
 
-		std::cout << "El Test: " << tipoTest << ", y el resultado por Coincidencias es: " << pertenece << ", y por Proximidad es: " << perteneceEuclidea << std::endl;
+		//std::cout << "El Test: " << tipoTest << ", y el resultado por Coincidencias es: " << pertenece << ", y por Proximidad es: " << perteneceEuclidea << std::endl;
+		std::cout << "El Test: " << tipoTest << ", y el resultado por Coincidencias es: " << perteneceKVecinos << std::endl;
 	}
 	fichTest.close();
 
@@ -131,7 +148,7 @@ void compararHistogramas(int *hisTest, int *hisTraining, long &totalCoincidencia
 }
 
 
-void distanciaEuclidea(int *hisTest, int *hisTraining, double &min, std::string &perteneceEuclidea, std::string tipoTraining)
+double distanciaEuclidea(int *hisTest, int *hisTraining, double &min, std::string &perteneceEuclidea, std::string tipoTraining)
 {
 	double resultado = 0;
 	///////////////////////////////// DISTANCIA EUCLIDEA /////////////////////////////////////////////	
@@ -143,19 +160,102 @@ void distanciaEuclidea(int *hisTest, int *hisTraining, double &min, std::string 
 		min = resultado;
 		perteneceEuclidea = tipoTraining;
 	}
-	
+	return resultado;
 }
 
+void encuentraKVecinos(Vecinos *vVecinos, int &posVecinos, const std::string tTraining, const double res)
+{
+	if(posVecinos < KVECINOS)
+	{
+		vVecinos[posVecinos].distancia = res;
+		vVecinos[posVecinos++].tipoVecino = tTraining;
+	}
+	else
+	{
+		for (int i = 0; i < KVECINOS; i++)
+			if (vVecinos[i].distancia > res)
+			{
+				vVecinos[i].distancia = res;
+				vVecinos[i].tipoVecino = tTraining;
 
-void guardaResultado(std::string img, std::string tipoMayorCoincidencia, std::string tipoMasProximo)
+				break;	//evitamos que modifique mas de un vecino
+			}
+	}
+}
+
+std::string votaVecino(Vecinos *vecinosMasProximos)
+{
+	Vecinos votacion[4];
+	for (int i = 0; i < 4; i++) votacion[i].distancia = 0;
+	votacion[0].tipoVecino = "SALINA.";
+	votacion[1].tipoVecino = "NO EMPAREJADO";
+	votacion[2].tipoVecino = "NO CONDICIONADO";
+	votacion[3].tipoVecino = "CONDICIONADO.";
+
+	for (int i = 0; i < KVECINOS; i++)
+	{
+		if(vecinosMasProximos[i].tipoVecino == "SALINA.") votacion[0].distancia++;
+		if(vecinosMasProximos[i].tipoVecino == "NO EMPAREJADO") votacion[1].distancia++;
+		if(vecinosMasProximos[i].tipoVecino == "NO CONDICIONADO") votacion[2].distancia++;
+		if(vecinosMasProximos[i].tipoVecino == "CONDICIONADO.") votacion[3].distancia++;
+	}
+
+	bool iguales = false;
+	int max = -1, posicion = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		if(votacion[i].distancia > max)
+		{
+			max = votacion[i].distancia;
+			posicion = i;
+			iguales = false;
+		}
+		else if(votacion[i].distancia == max) iguales = true;
+	}
+	std::cout << "VOTACIÓN VECINOS: " << std::endl;
+	for (int i = 0; i < 4; i++) std::cout << "\t" << votacion[i].tipoVecino << " " << votacion[i].distancia << " votos" << std::endl;
+	std::cout << "POSICION: " << posicion << std::endl;
+	std::cout << "\t______________________________________________" << std::endl;
+
+	if (iguales)
+	{
+		std::string tipoDefinitivo;
+		int minDistanciaDefinitiva = -1;
+		for (int i = posicion; i < 4; i++)
+		{
+			if(votacion[i].distancia == max)
+			{
+				for(int j = 0; j < KVECINOS; j++)
+				{
+					if ( (votacion[i].tipoVecino == vecinosMasProximos[j].tipoVecino) and (vecinosMasProximos[j].distancia < minDistanciaDefinitiva or minDistanciaDefinitiva == -1))
+					{
+						minDistanciaDefinitiva = vecinosMasProximos[j].distancia;
+						tipoDefinitivo = vecinosMasProximos[j].tipoVecino;
+					}
+				}
+			}
+		}
+		return tipoDefinitivo;
+	}
+
+	return votacion[posicion].tipoVecino;
+}
+
+void imprimeKVecinos(Vecinos *vecinosMasProximos)
+{
+	for (int i = 0; i < KVECINOS; ++i) std::cout << "\t Tipo: " << vecinosMasProximos[i].tipoVecino << ", y distancia = " << vecinosMasProximos[i].distancia << std::endl;
+	std::cout << "\t______________________________________________" << std::endl;
+}
+
+void guardaResultado(std::string img, std::string tipoMayorCoincidencia, std::string tipoMasProximo, std::string tipoKVecino)
 {
 	//Salvar el histograma
 	{		
 	    // Se crea un archivo de salida
-	    std::ofstream ofs("ResultadosExperimento.txt", std::ios::app);
+	    std::ofstream ofs("ResultadosExperimentoKVecinos.txt", std::ios::app);
 	    boost::archive::text_oarchive ar(ofs);
 
 	    // Escribe el nombre de la imagen, el tipo al que pertenece y su histograma
-	    ar & img & tipoMayorCoincidencia & tipoMasProximo;
+	    ar & img /*& tipoMayorCoincidencia & tipoMasProximo;*/ & tipoKVecino;
 	}
 }
